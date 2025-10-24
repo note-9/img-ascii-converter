ba1435687b3baac8d6cb8bd37578a4a416915a2d
note-9 <heemanshbhawsar2004@gmail.com>
2025-10-24 21:05:11 +0530

    file opening

--- main.go
+++ main.go
@@ -3,4 +3,17 @@ package main
 import (
     "fmt"
     "image"
+    _ "image/jpeg"
+    _ "image/png"
+    "os"
 )
+
+func main() {
+    filePath := "~/Pictures/girl.jpg"
+    f, err := os.Open(filePath)
+    if err != nil {
+        fmt.Printf("Error opening file %v\n", err)
+        return
+    }
+    
+}
