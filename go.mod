f019ba64598243a4aa16978a477586e0f16c52bb
note-9 <heemanshbhawsar2004@gmail.com>
2025-10-24 23:47:44 +0530

    still not there yet

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/note-9/img-ascii-converter
 
 go 1.25.3
+
+require (
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	golang.org/x/term v0.36.0
+)
+
+require golang.org/x/sys v0.37.0 // indirect
--- main.go
+++ main.go
@@ -3,17 +3,69 @@ package main
 import (
     "fmt"
     "image"
+	"image/color"
     _ "image/jpeg"
     _ "image/png"
     "os"
+	"github.com/nfnt/resize"
+	"golang.org/x/term"
 )
 
 func main() {
-    filePath := "~/Pictures/girl.jpg"
+    filePath := "/home/note/Pictures/Wallpaper/wallhaven-6dr6rx_1920x1080.png"
     f, err := os.Open(filePath)
     if err != nil {
         fmt.Printf("Error opening file %v\n", err)
         return
     }
-    
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil{
+		fmt.Printf("Error decoding image %v\n", err)
+		return
+	}
+	grayImg := image.NewGray(img.Bounds())
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			grayImg.Set(x, y, img.At(x, y))
+		}
+	}
+	
+	if !term.IsTerminal(int(os.Stdout.Fd())) {
+		fmt.Println("Stdout is not a terminal.")
+		return
+	}
+
+	// Get the terminal size
+	width, _, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		fmt.Printf("Error getting terminal size: %v\n", err)
+		return
+	}
+	
+	newWidth := uint(width)
+	newHeight := uint(0) 
+
+	m := resize.Resize(newWidth, newHeight, img, resize.Lanczos3)//lanczos3 interpolation
+	
+	charset := " .,-~+=oO@"
+
+	ascii := ""
+
+	for y := m.Bounds().Min.Y; y < m.Bounds().Max.Y; y++ {
+		for x := m.Bounds().Min.X; x < m.Bounds().Max.X; x++ {
+			c := color.GrayModel.Convert(img.At(x, y)).(color.Gray)
+			brightness := c.Y 
+			
+			charIndex := int(float64(brightness) / 256.0 * float64(len(charset)))
+			if charIndex >= len(charset) {
+				charIndex = len(charset) - 1
+			}
+			
+			ascii += string(charset[charIndex])
+		}
+		ascii += "\n"
+	}
+	fmt.Println(ascii)
 }
